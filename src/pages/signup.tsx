479fd329b929bcb626c93b4355198f96e35b9afe
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-11 21:47:34 -0300

    mew

---
+++ src/pages/signup.tsx
@@ -0,0 +1,21 @@
+import Styles from "@/scss/Login.module.scss";
+
+export default function Signup(){
+
+
+    
+    return(
+        <>  
+            <div className={Styles.loginOut}>
+                <form className={Styles.form}>
+                    <h1 className={Styles.title}>Signup</h1>
+                    <h3 className={Styles.subtitle}>E-mail</h3>
+                    <input id="email" className={Styles.inp} type="email" placeholder="exemplo@gmail.com"/>
+                    <h3 className={Styles.subtitle}>Senha</h3>
+                    <input id="password" className={Styles.inp} type="password" placeholder="123456"/>
+                    <button className={Styles.btForm}>Entrar</button>
+                </form>
+            </div>
+        </>
+    )
+}
