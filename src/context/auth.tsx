afa0d8369da89605ce1cb514d1a7e8a612de53ab
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-12 13:58:15 -0300

    new

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -4,27 +4,10 @@ export const AuthContext = createContext<any>(()=>{})
 
 export const AuthProvider = ({ children }: any) => {
 
-    const [token, setToken] = useState<string>(localStorage.getItem('token') || '');
-
-    useEffect(() => {
-        localStorage.setItem('token', token);
-      }, [token]);
-
-    const login = (token:any) => {
-        setToken(token);
-    };
-
-    const logout = () => {
-        setToken('');
-        localStorage.removeItem('token');
-    };
 
     return(
         <AuthContext.Provider value={{
-            token,
-            login,
-            setToken,
-            logout
+
             }}> 
             {children}
         </AuthContext.Provider>
