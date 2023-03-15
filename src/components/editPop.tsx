ca4719b5789fac741ea7b190cc24b986c45ebe9a
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-15 07:55:13 -0300

    new

---
+++ src/components/editPop.tsx
@@ -0,0 +1,87 @@
+import { PopupContext } from "@/context/popup";
+import { useContext, useState } from "react";
+
+const Popup = (): JSX.Element => {
+  const { edit, cover, addItem } = useContext(PopupContext); // importando a função addItem
+  const [code, setCode] = useState("");
+  const [product, setProduct] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newItem = {
+      codigo: parseInt(code),
+      nome: product,
+      quantidade: parseInt(quantity),
+      value: parseInt(price),
+      createDate: new Date().toISOString(),
+    };
+
+    addItem(newItem);
+
+    setCode("");
+    setProduct("");
+    setQuantity("");
+    setPrice("");
+  };
+
+  return (
+    <div className={edit}>
+      <div className="Popup">
+        <div>
+          <p>Produto</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="code">Codigo:</label>
+            <input
+              type="number"
+              id="code"
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="product">Produto:</label>
+            <input
+              type="text"
+              id="product"
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="quantity">Quantidade:</label>
+            <input
+              type="number"
+              id="quantity"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="price">Preço:</label>
+            <input
+              type="number"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Adicionar</button>
+            <button onClick={() => changePopState()}>Cancelar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
--- src/context/popup.tsx
+++ src/context/popup.tsx
@@ -5,8 +5,10 @@ export const PopupContext = createContext<any>(()=>{})
 export const PopupProvider = ({ children }: any) => {
 
     const [cover, setCover] = useState("Cover off")
-    const [edit, setEdit ] = useState(false)
+    const [edit, setEdit ] = useState("Edit off")
     const [data, setData] = useState([{"codigo":1,"nome":"Item 1","quantidade":10,"value":100,"createDate":"2023-03-13T07:06:29.253Z"},{"codigo":2,"nome":"Item 2","quantidade":20,"value":200,"createDate":"2023-03-13T07:06:29.253Z"},{"codigo":3,"nome":"Item 3","quantidade":30,"value":300,"createDate":"2023-03-13T07:06:29.253Z"},{"codigo":4,"nome":"Item 4","quantidade":40,"value":400,"createDate":"2023-03-13T07:06:29.253Z"},{"codigo":5,"nome":"Item 5","quantidade":50,"value":500,"createDate":"2023-03-13T07:06:29.253Z"}])
+
+
 
     const changePopState = () => {
         if(cover === "Cover"){
